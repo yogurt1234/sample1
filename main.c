b79eb2230e87e25a728b0a43a34445040b2cf8ac
yogurt1234 <kuriharalaboratory@yahoo.co.jp>
2021-01-15 09:54:44 +0900

    while loop

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 
 int main()
 {
-    int i = 13;
-
-    printf("hello world %d", i);
+    int i = 0;
+    while (i < 10)
+    {
+        printf("hello world %d", i);
+        i++;
+    }
 
     return 0;
 }
